609da5e0b06339f1e7861bf9c1774ee3d7547126
L-Greco <makaronaskonstandinos@gmail.com>
2021-05-12 14:39:20 +0300

    Finished the main Post , started the Modal

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,71 @@
 import { Component } from "react";
 import "../Post.css";
+import PhotoSizeSelectActualOutlinedIcon from "@material-ui/icons/PhotoSizeSelectActualOutlined";
+import PlayArrowOutlinedIcon from "@material-ui/icons/PlayArrowOutlined";
+import EventNoteRoundedIcon from "@material-ui/icons/EventNoteRounded";
+import AssignmentRoundedIcon from "@material-ui/icons/AssignmentRounded";
 
 class Post extends Component {
+  state = {};
   render() {
-    return <></>;
+    return (
+      <div className="postParent">
+        <div
+          style={{
+            flexWrap: "nowrap",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+          className="mt-2 mb-2"
+        >
+          <img className="postImg" src={"https://picsum.photos/200/300"} />
+          <button className="postBtn">Start a post</button>
+        </div>
+        <div
+          style={{
+            flexWrap: "nowrap",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <span className="link">
+            {
+              <PhotoSizeSelectActualOutlinedIcon
+                style={{ color: "rgb(112,181,249)" }}
+              />
+            }{" "}
+            Photo{" "}
+          </span>
+          <span className="link">
+            {" "}
+            {
+              <PlayArrowOutlinedIcon
+                style={{
+                  color: "white",
+                  backgroundColor: "rgb(127,193,94)",
+                  borderRadius: "5px",
+                }}
+              />
+            }{" "}
+            Video
+          </span>
+          <span className="link">
+            {" "}
+            {<EventNoteRoundedIcon style={{ color: "rgb(231,163,62)" }} />}{" "}
+            Event{" "}
+          </span>
+          <span className="link">
+            {" "}
+            {
+              <AssignmentRoundedIcon style={{ color: "rgb(245,152,126)" }} />
+            }{" "}
+            Write article{" "}
+          </span>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/components/PostModal.jsx
@@ -0,0 +1,10 @@
+import "../Post.css";
+import { Component } from "react";
+
+class PostModal extends Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default PostModal;
